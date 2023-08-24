64f363d1fde5bec82a08caab6c2302d56c2f4d80
KKorzec <konradkorzec99@gmail.com>
2023-08-24 20:20:49 +0200

    Added info-blocks in index.js page

---
+++ StronkaBITEHack/src/components/infoBlock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/global.css';
+
+const InfoBlock = ({ imageSrc, title, content }) => {
+    return (
+        <div className="info-block">
+            <div className="info-image">
+                <img src={imageSrc} alt={`Opis ${title}`} />
+            </div>
+            <div className="info-text">
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </div>
+        </div>
+    );
+};
+
+export default InfoBlock;
--- StronkaBITEHack/src/pages/index.js
+++ StronkaBITEHack/src/pages/index.js
@@ -2,20 +2,31 @@ import React from 'react';
 import '../styles/global.css';
 import Seo from "../components/seo";
 import rafon from "../images/Rafonix - przejebane życie.mp4";
+import rafonIN from "../images/rafon_index.jpg";
 import VideoPlayer from "../components/videoPlayerHomePage";
 import Header from "../components/header";
+import InfoBlock from "../components/InfoBlock"
 
 const HomePage = () => {
     return (
         <div>
             <Header/>
-            <div  className={"container"}>
+            <div className={"container"}>
                 <div className={"video-container"}>
                     <VideoPlayer videoSrc={rafon} />
                 </div>
                 <div className={"content-container"}>
                     <h1>Witaj na stronie!</h1>
-                    <h3>Tutaj będzie treść</h3>
+                    <InfoBlock
+                        imageSrc={rafonIN}
+                        title="Kiedy?"
+                        content="20-21 stycznia 2k24"
+                    />
+                    <InfoBlock
+                        imageSrc={rafonIN}
+                        title="Gdzie?"
+                        content="Klub studio"
+                    />
                 </div>
             </div>
         </div>
