330371f5d39243885d6256285a0e2fedc4288d93
KKorzec <konradkorzec99@gmail.com>
2023-09-10 19:32:59 +0200

    Added navigation dots

--- StronkaBITEHack/src/pages/index.js
+++ StronkaBITEHack/src/pages/index.js
@@ -32,27 +32,74 @@ const HomePage = () => {
         sectionRefs.main.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
     }, []);
 
-     const { images, currentImage, handleImageChange } = ImageData();
+    const { images, currentImage, handleImageChange } = ImageData();
+
+    const handleDotClick = (sectionName) => {
+        const section = sectionRefs[sectionName].current;
+        section.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    useEffect(() => {
+        const dots = document.querySelectorAll(".dot");
+
+        dots.forEach((dot) => {
+            const sectionName = dot.getAttribute("data-section");
+            dot.addEventListener("click", () => {
+                handleDotClick(sectionName);
+            });
+        });
+
+        window.addEventListener("scroll", () => {
+            const scrollPosition = window.scrollY;
+            const sections = Object.values(sectionRefs);
+
+            sections.forEach((sectionRef) => {
+                const section = sectionRef.current;
+                if (!section) return;
+
+                const sectionTop = section.offsetTop;
+                const sectionBottom = sectionTop + section.clientHeight;
+
+                if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                    const sectionName = section.getAttribute("data-section");
+                    dots.forEach((dot) => {
+                        dot.classList.remove("active");
+                        if (dot.getAttribute("data-section") === sectionName) {
+                            dot.classList.add("active");
+                        }
+                    });
+                }
+            });
+        });
+    }, [sectionRefs]);
 
     return (
         <div>
             <Header scrollToSection={scrollToSection} />
-            <section ref={sectionRefs.main}>
+            <div id="scrollbar" className="scrollbar">
+                <div className="dot" data-section="main"></div>
+                <div className="dot" data-section="about1"></div>
+                <div className="dot" data-section="about2"></div>
+                <div className="dot" data-section="about3"></div>
+                <div className="dot" data-section="about4"></div>
+                <div className="dot" data-section="organizers"></div>
+            </div>
+            <section ref={sectionRefs.main} data-section="main">
                 <MainPage/>
             </section>
-            <section ref={sectionRefs.about1}>
+            <section ref={sectionRefs.about1} data-section="about1">
                 <About1/>
             </section>
-            <section ref={sectionRefs.about2}>
+            <section ref={sectionRefs.about2} data-section="about2">
                 <About2/>
             </section>
-            <section ref={sectionRefs.about3}>
+            <section ref={sectionRefs.about3} data-section="about3">
                 <About3/>
             </section>
-            <section ref={sectionRefs.about4}>
+            <section ref={sectionRefs.about4} data-section="about4">
                 <About4/>
             </section>
-            <section ref={sectionRefs.organizers}>
+            <section ref={sectionRefs.organizers} data-section="organizers">
                 <Organizers images={images} currentImage={currentImage} handleImageChange={handleImageChange} />
             </section>
         </div>
