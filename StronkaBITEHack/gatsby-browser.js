1f6f92bf0aacbf65d066ab96d9444058002b0e62
KKorzec <konradkorzec99@gmail.com>
2023-10-07 18:56:27 +0200

    Draft of 404 rerouting

--- StronkaBITEHack/gatsby-browser.js
+++ StronkaBITEHack/gatsby-browser.js
@@ -4,4 +4,14 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+import Redirect404 from '../StronkaBITEHack/src/components/redirect404';
+
+export const wrapPageElement = ({ element }) => {
+    return (
+        <>
+            {element}
+            <Redirect404 />
+        </>
+    );
+};
